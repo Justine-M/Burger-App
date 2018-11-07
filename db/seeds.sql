67b47485ae12490b027e85e86b84572bbe89b9c1
Justine-M <justalexmays@hotmail.com>
2018-11-07 17:07:19 -0500

    first

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (Burger_Name, Devoured) VALUES ('Bacon Cheeseburger', true);
+INSERT INTO burgers (Burger_Name, Devoured) VALUES ('Veggie Burger', true);
+INSERT INTO burgers (Burger_Name, Devoured) VALUES ('Turkey Burger', true);
